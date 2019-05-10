e4a654a6c5f51b3c94dde00dcd01b1b2794ce641
Ray Wong <raywong.hust@gmail.com>
2019-05-11 00:08:07 +0800

    rename a function

--- util/util.py
+++ util/util.py
@@ -1,9 +1,5 @@
-import torch
 from PIL import Image
-import inspect, re
 import numpy as np
-import os
-import collections
 
 from skimage.draw import circle, line_aa, polygon
 
@@ -33,7 +29,7 @@ MISSING_VALUE = -1
 
 
 def map_to_cord(pose_map, threshold=0.1):
-    all_peaks = [[] for i in range(18)]
+    all_peaks = [[] for _ in range(18)]
     pose_map = pose_map[..., :18]
 
     y, x, z = np.where(np.logical_and(pose_map == pose_map.max(axis = (0, 1)),
@@ -55,7 +51,7 @@ def map_to_cord(pose_map, threshold=0.1):
     return np.concatenate([np.expand_dims(y_values, -1), np.expand_dims(x_values, -1)], axis=1)
 
 
-def draw_pose_from_map_(pose_map, threshold=0.1, **kwargs):
+def draw_pose_from_map(pose_map, threshold=0.1, **kwargs):
     # CHW -> HCW -> HWC
     pose_map = pose_map.cpu().transpose(1, 0).transpose(2, 1).numpy()
 
@@ -87,61 +83,6 @@ def draw_pose_from_cords(pose_joints, img_size, radius=2, draw_joints=True):
 
     return colors, mask
 
-
-def diagnose_network(net, name='network'):
-    mean = 0.0
-    count = 0
-    for param in net.parameters():
-        if param.grad is not None:
-            mean += torch.mean(torch.abs(param.grad.data))
-            count += 1
-    if count > 0:
-        mean = mean / count
-    print(name)
-    print(mean)
-
-
 def save_image(image_numpy, image_path):
     image_pil = Image.fromarray(image_numpy)
     image_pil.save(image_path)
-
-
-def info(object, spacing=10, collapse=1):
-    """Print methods and doc strings.
-    Takes module, class, list, dictionary, or string."""
-    methodList = [e for e in dir(object) if isinstance(getattr(object, e), collections.Callable)]
-    processFunc = collapse and (lambda s: " ".join(s.split())) or (lambda s: s)
-    print( "\n".join(["%s %s" %
-                     (method.ljust(spacing),
-                      processFunc(str(getattr(object, method).__doc__)))
-                     for method in methodList]) )
-
-
-def varname(p):
-    for line in inspect.getframeinfo(inspect.currentframe().f_back)[3]:
-        m = re.search(r'\bvarname\s*\(\s*([A-Za-z_][A-Za-z0-9_]*)\s*\)', line)
-        if m:
-            return m.group(1)
-
-
-def print_numpy(x, val=True, shp=False):
-    x = x.astype(np.float64)
-    if shp:
-        print('shape,', x.shape)
-    if val:
-        x = x.flatten()
-        print('mean = %3.3f, min = %3.3f, max = %3.3f, median = %3.3f, std=%3.3f' % (
-            np.mean(x), np.min(x), np.max(x), np.median(x), np.std(x)))
-
-
-def mkdirs(paths):
-    if isinstance(paths, list) and not isinstance(paths, str):
-        for path in paths:
-            mkdir(path)
-    else:
-        mkdir(paths)
-
-
-def mkdir(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
--- util/v.py
+++ util/v.py
@@ -21,8 +21,8 @@ def get_current_visuals_(img_path, data_pair, new_imgs=None):
         input_P1 = util.tensor2image(data_pair["P1"].data[i])
         input_P2 = util.tensor2image(data_pair["P2"].data[i])
         input_p2_mask = util.tensor2image(data_pair["MP2"].data[i])
-        input_BP1 = util.draw_pose_from_map_(data_pair["BP1"].data[i])[0]
-        input_BP2 = util.draw_pose_from_map_(data_pair["BP2"].data[i])[0]
+        input_BP1 = util.draw_pose_from_map(data_pair["BP1"].data[i])[0]
+        input_BP2 = util.draw_pose_from_map(data_pair["BP2"].data[i])[0]
 
         make_vis([input_P1, input_BP1, input_P2, input_BP2, input_p2_mask] + new_img_list, i)
 
@@ -48,8 +48,8 @@ def get_current_visuals(img_path, data_pair, generated_img1, generated_img2=None
         input_P2 = util.tensor2image(data_pair["P2"].data[i])
         input_p2_mask = util.tensor2image(data_pair["MP2"].data[i])
         fake_p1 = util.tensor2image(generated_img1.data[i])
-        input_BP1 = util.draw_pose_from_map_(data_pair["BP1"].data[i])[0]
-        input_BP2 = util.draw_pose_from_map_(data_pair["BP2"].data[i])[0]
+        input_BP1 = util.draw_pose_from_map(data_pair["BP1"].data[i])[0]
+        input_BP2 = util.draw_pose_from_map(data_pair["BP2"].data[i])[0]
 
         if generated_img2 is None:
             make_vis([input_P1, input_BP1, input_P2, input_BP2, input_p2_mask, fake_p1], i)
